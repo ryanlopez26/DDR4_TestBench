dcec7284b4e1636584f864bf6abe7834fc0825e9
ryanlopez26 <ryl26@pitt.edu>
2026-05-27 11:00:25 -0400

    Correct path to server exe

--- EDF/build_image.sh
+++ EDF/build_image.sh
@@ -1,5 +1,5 @@
 # Copy built server executable into the needed bblayer
-cp -f zcu_app/zcu_server EDF/sources/meta-zcu104-ddr4/recipes-apps/zcu-server/files/
+cp -f ../../zcu_app/zcu_server EDF/sources/meta-zcu104-ddr4/recipes-apps/zcu-server/files/
 
 # Build the image
 MACHINE=zynqmp-zcu104-sdt-full bitbake core-image-minimal
