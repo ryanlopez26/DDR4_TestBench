81abe6299159021a0f5dc77c822d675083fee55c
Rylo <ryan@lopezes.org>
2026-05-28 04:55:32 -0400

    Revert "Added new scripts and rebuilt fpga image"
    This reverts commit 4439ab60b12659496c7329b4b52875b75a379285.

--- EDF/build/conf/DRVLISTConfig.cmake
+++
@@ -1,1 +0,0 @@
-set(DRIVER_LIST avbuf;axipmon;canps;clockps;common;csudma;ddrcpsu;dpdma;dppsu;emacps;gpiops;iicps;ipipsu;qspipsu;resetps;rtcpsu;sdps;sysmonpsu;ttcps;uartps;usbpsu;video_common;wdtps;zdma)
--- Vivado/extract_xsa.sh
+++
@@ -1,3 +0,0 @@
-source /tools/vivado/2025.2/Vitis/settings64.sh
-rm -rf sdt_output/
-sdtgen -xsa System_wrapper.xsa -dir sdt_output/
