17e5257ac3265aee1b3100e750e32f9e08f73d5d
Rylo <ryan@lopezes.org>
2026-05-28 04:43:55 -0400

    Scripts and Vivado updates

--- EDF/generate_machine_conf.sh
+++ EDF/generate_machine_conf.sh
@@ -3,6 +3,6 @@ source sources/poky/oe-init-build-env build/
 
 # now you're inside build/ — paths shift one level deeper
 gen-machineconf --soc-family zynqmp \
-                --hw-description ../../Vivado/sdt_output \
-                --machine-name zynqmp-zcu104-sdt-full \
-                --output conf
+    --hw-description ../../Vivado/sdt_output \
+    --machine-name zynqmp-zcu104-ddr4 \
+    --output conf
